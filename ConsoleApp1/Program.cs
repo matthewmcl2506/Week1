ca09c8f42b28f27cc0bf581bee8dc5e799a03237
MM <McLearnon-M1@ulster.ac.uk>
2025-09-29 17:54:28 +0100

    This is the first attempt at the week 1 practica task 1l.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using System.IO.Pipes;
+using System.Linq.Expressions;
+
+class Week1Practical
+{
+    public static void Main(string[] args) {
+        string name = getName();
+        int age = getAge();
+    }
+
+    static string getName()
+    {
+        string name;
+
+        while (true)
+        {
+            Console.WriteLine("Please enter your name below:\n");
+            name = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(name) &&  name.All(c => char.IsLetter(c) || c == ' '))
+            {
+                return name;
+            }
+
+            Console.WriteLine("Invalid input, please try again:\n");
+        }
+    }
+    static int getAge()
+    {
+
+        while (true)
+        {
+            Console.WriteLine("Please enter your age below:\n");
+            String age = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(age) && (int.TryParse(age, out int ageConvert) && ageConvert > 0))
+            {
+                return ageConvert;
+            }
+
+                Console.WriteLine("Invalid input, please try again:\n");
+        }
+    }
+
+}
