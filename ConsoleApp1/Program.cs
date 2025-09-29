1a0bf40f87825eecc7d4d25f1dfc7710672c2fc1
MM <McLearnon-M1@ulster.ac.uk>
2025-09-29 18:07:02 +0100

    Final version of week 1 practical with some modifications.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,38 @@ class Week1Practical
     public static void Main(string[] args) {
         string name = getName();
         int age = getAge();
+        if (age < 13)
+        {
+            Console.WriteLine("You are {0} years old, therefore you are a child.", age);
+        }
+        else if (age >= 13 && age <=19)
+        {
+            Console.WriteLine("You are {0} years old, therefore you are a teenager.", age);
+        }
+        else
+        {
+            Console.WriteLine("You are {0} years old, therefore you are an adult.", age);
+        }
+        int year = System.DateTime.Now.Year;
+        bool leapYear = false; ;
+        if (((year - age) % 4 == 0) && ((year - age) % 100 != 0))
+        {
+            Console.WriteLine("You were born on a leap year");
+        }
+        else
+        {
+            Console.WriteLine("You were not born on a leap year");
+        }
+        if ((year - age) % 2 == 0)
+        {
+            Console.WriteLine("You were born on a year that is an even number");
+        }
+        else
+        {
+            Console.WriteLine("You were born on a year that is an odd number");
+        }
+
+
     }
 
     static string getName()
@@ -33,7 +65,7 @@ class Week1Practical
             Console.WriteLine("Please enter your age below:\n");
             String age = Console.ReadLine();
 
-            if (!string.IsNullOrWhiteSpace(age) && (int.TryParse(age, out int ageConvert) && ageConvert > 0))
+            if (!string.IsNullOrWhiteSpace(age) && (int.TryParse(age, out int ageConvert) && ageConvert > 0 && ageConvert < 110))
             {
                 return ageConvert;
             }
